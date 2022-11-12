9cfdca637949925655e5379e5bfe54532eb51096
Harihara sudhan <nhariharasudhan7@gmail.com>
2022-11-13 01:44:15 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+})
+
+
+app.listen(3000 , ()=>{
+    console.log("Listening on port 3000")
+})
