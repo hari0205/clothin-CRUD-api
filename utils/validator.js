3eb8a1cf88cce5fb55cf03d24345635860ccfc7b
Harihara sudhan <nhariharasudhan7@gmail.com>
2022-11-13 19:17:52 +0530

    Added user validator check

--- utils/validator.js
+++ utils/validator.js
@@ -1,1 +1,14 @@
 const Joi = require('joi')
+const jwt = require('jsonwebtoken')
+
+
+
+const validateSignup =(data)=>{
+    const schema = Joi.object({
+        name: Joi.string().min(4).required(),
+        email: Joi.string().required().email(),
+        password: Joi.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/)
+      })
+
+    return schema.validate(data)
+}
