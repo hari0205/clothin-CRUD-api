776069cb2f15f87a806a7b95dbe960def8524c43
Harihara sudhan <nhariharasudhan7@gmail.com>
2022-11-13 21:43:13 +0530

    Added routes and mongoDB connection

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
-const express = require("express");
+const express = require("express")
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const validator=require("./utils/validator")
+
+
 
 
 const app = express();
+
+
+
+
+dotenv.config();
+
+
+function init(params) {
+    mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    mongoose.connection.on("connected", (err, res) => {
+    console.log("Connected to database.");
+ });
+     mongoose.connection.on("error", (err) => {
+     console.log("err", err);
+});
+
+}
+
+init()
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -14,3 +37,6 @@ app.listen(3000 , (err)=>{
         console.log("Listening on port 3000")
     }
 })
+
+
+app.use("/api/clothes", validator.verifyToken,  ClothesRoute);
